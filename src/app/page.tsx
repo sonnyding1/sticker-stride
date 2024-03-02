bc50f2afab13714d777eea53caa3af9e75c051ed
Recogn1z <543257239@qq.com>
2024-03-01 23:26:48 -0800

    reorder directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import CustomCalendar from "./components/calendar";
+import CustomCalendar from "../components/events/calendar";
 
 export default function HomePage() {
   return (
     <div className="h-[600px]">
       <CustomCalendar />
     </div>
-  )
+  );
 }
